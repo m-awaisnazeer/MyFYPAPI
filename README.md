de43d8a1394911b24c266a79c855486ea4085f7f
Awais <m.awaisnaxeer@gmail.com>
2020-11-24 18:51:38 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# MyRestaurantMySQLAPI
+
+
+# MyFYPAPI
---
+++ app.js
@@ -0,0 +1,32 @@
+const PORT = 3000
+
+var express = require('express')
+var app = express()
+var mysql = require('mysql')
+var myconnection = require('express-myconnection')
+var bodyParser = require('body-parser')
+
+var config = require('./db')
+
+var dbOptions = {
+    host: config.database.host,
+    user: config.database.user,
+    password: config.database.password,
+    port: config.database.port,
+    database: config.database.db
+}
+
+var routes = require('./routes/index')
+var publicDir = (__dirname + '/public/'); //set static image for display image
+
+app.use(express.static(publicDir))
+app.use(myconnection(mysql, dbOptions, 'pool'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+
+app.use("/", routes)
+app.listen(PORT, () => {
+    console.log('BACKEND running on port ' + PORT)
+})
+
