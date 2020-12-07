25e7f7e664dfcf430b28576cd2cd84f7b6ad3cce
Awais <m.awaisnaxeer@gmail.com>
2020-12-07 16:06:59 +0500

    view order details

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,74 @@ router.get('/', function (req, res, next) {
     res.send('Hello world')
 })
 
+//======================================
+//RESTAURANTOWNER TABLE
+//GET /POST
+//======================================
+
+router.get('/restaurantowner', function (req, res, next) {
+    if (req.query.key == API_KEY) {
+        var fbid = req.query.fbid
+
+        if (fbid != null) {
+            req.getConnection(function (error, conn) {
+                conn.query('SELECT userPhone,name,CASE WHEN status=0 THEN \'FALSE\' ELSE \'TRUE\' END as status,restaurantId,fbid FROM restaurantowner WHERE fbid=?', [fbid], function (err, rows, fields) {
+                    if (err) {
+                        res.status(500)
+                        res.send(JSON.stringify({ success: false, message: err.message }))
+                    }
+                    else {
+                        if (rows.length > 0) {
+                            res.send(JSON.stringify({ success: true, result: rows }))
+
+                        } else {
+                            res.send(JSON.stringify({ success: false, message: "_EMPTY" }))
+                        }
+                    }
+                })
+            })
+        }
+        else {
+            res.send(JSON.stringify({ success: false, message: "Missing fbid in query" }))
+        }
+    }
+    else {
+        res.send(JSON.stringify({ success: false, message:"Wrong API Key" }))
+    }
+})
+
+router.post('/restaurantowner', function (req, res, next) {
+    if (req.body.key == API_KEY) {
+
+        var fbid = req.body.fbid
+        var user_phone = req.body.userPhone
+        var user_name = req.body.userName
+
+        if (fbid != null) {
+            req.getConnection(function (error, conn) {
+                conn.query('INSERT INTO restaurantowner(FBID,UserPhone,Status,Name) VALUES(?,?,?,?) ON DUPLICATE KEY UPDATE Name=?', [fbid, user_phone, 0, user_name, user_name], function (err, rows, fields) {
+                    if (err) {
+                        res.status(500)
+                        res.send(JSON.stringify({ success: false, message: err.message }))
+                    }
+                    else {
+                        if (rows.affectedRows > 0) {
+                            res.send(JSON.stringify({ success: true, message:"Success" }))
+
+                        }
+                    }
+                })
+            })
+        }
+        else {
+            res.send(JSON.stringify({ success: false, message: "Missing fbid in body" }))
+        }
+    }
+    else {
+        res.send(JSON.stringify({ success: false, message: "Wrong API Key" }))
+    }
+})
+
 
 //======================================
 //USER TABLE
@@ -255,7 +323,7 @@ router.get('/restaurantById', function (req, res, next) {
     if (req.query.key == API_KEY) {
 
         req.getConnection(function (error, conn) {
-            var restaurant_id = req.body.restaurantId
+            var restaurant_id = req.query.restaurantId
             if (restaurant_id != null) {
                 conn.query('SELECT id,name,address,phone,lat,lng,userOwner,image,paymentUrl FROM restaurant WHERE id=?', [restaurant_id], function (err, rows, fields) {
                     if (err) {
@@ -290,16 +358,16 @@ router.get('/nearbyrestaurant', function (req, res, next) {
         var user_lng = parseFloat(req.query.lng)
         var distance = parseFloat(req.query.distance)
 
-        if (user_lat != Number.NaN && user_lng != Number.NaN) {
+        if (user_lat != Number.Nan && user_lng != Number.Nan) {
 
             req.getConnection(function (error, conn) {
                 conn.query('SELECT * FROM (SELECT id,name,address,phone,lat,lng,userOwner,image,paymentUrl,'
                     + 'ROUND(111.045 * DEGREES(ACOS(COS(RADIANS(?)) * COS(RADIANS(lat))'
                     + '* COS(RADIANS(lng) - RADIANS(?)) + SIN(RADIANS(?))'
-                    + '* SIN(RADIANS(lat)))),2) AS distance_in_km FROM Restaurant)tempTable WHERE distance_in_km < ?', [user_lat, user_lng, user_lat, distance], function (err, rows, fields) {
+                    + '* SIN(RADIANS(lat)))),2) AS distance_in_km FROM restaurant)tempTable WHERE distance_in_km < ?', [user_lat, user_lng, user_lat, distance], function (err, rows, fields) {
                     if (err) {
                         res.status(500)
-                        res.send(JSON.stringify({ success: false, message: err.message }))
+                        res.send(JSON.stringify({ success: false, message: err.message+"500" }))
                     }
                     else {
                         if (rows.length > 0) {
@@ -547,16 +615,135 @@ router.get('/addon', function (req, res, next) {
 //GET / POST
 //======================================
 
+router.get('/orderbyrestaurant', function (req, res, next) {
+    if (req.query.key == API_KEY) {
+
+        req.getConnection(function (error, conn) {
+            var restaurantId = req.query.restaurantId
+            var startIndex = parseInt(req.query.from)
+            var endIndex = parseInt(req.query.to)
+
+            //set Default if user not pass params
+            if(isNaN(startIndex))
+                startIndex=0;
+
+            if(isNaN(endIndex))
+                endIndex=10;
+
+
+            if (restaurantId != null) {
+                conn.query('SELECT orderId,orderFBID,orderPhone,orderName,orderAddress,orderStatus,orderDate,'
+                    + 'restaurantId,transactionId,'
+                    + 'CASE WHEN cod=1 THEN \'TRUE\' ELSE \'FALSE\' END as cod,'
+                    + 'totalPrice,numOfItem FROM `order` WHERE restaurantId =? AND numOfItem > 0'
+                    + ' ORDER By orderId DESC LIMIT ?,?', [restaurantId,startIndex,endIndex], function (err, rows, fields) {
+                    if (err) {
+                        res.status(500)
+                        res.send(JSON.stringify({ success: false, message: err.message }))
+                    }
+                    else {
+                        if (rows.length > 0) {
+                            res.send(JSON.stringify({ success: true, result: rows }))
+
+                        } else {
+                            res.send(JSON.stringify({ success: false, message: "_EMPTY" }))
+                        }
+                    }
+                })
+            }
+            else {
+                res.send(JSON.stringify({ success: false, message: "order restaurantId missing" }))
+            }
+        })
+
+    }
+    else {
+        res.send(JSON.stringify({ success: false, message: "Wrong API Key" }))
+    }
+})
+
+router.get('/maxorderbyrestaurant', function (req, res, next) {
+    if (req.query.key == API_KEY) {
+
+        req.getConnection(function (error, conn) {
+            var restaurantId = req.query.restaurantId
+            
+            if (restaurantId != null) {
+                conn.query('SELECT COUNT(orderId) as maxRowNum  FROM `order` WHERE restaurantId =? AND numOfItem > 0'
+                    + ' ORDER By orderId DESC', [restaurantId], function (err, rows, fields) {
+                    if (err) {
+                        res.status(500)
+                        res.send(JSON.stringify({ success: false, message: err.message }))
+                    }
+                    else {
+                        if (rows.length > 0) {
+                            res.send(JSON.stringify({ success: true, result: rows }))
+
+                        } else {
+                            res.send(JSON.stringify({ success: false, message: "_EMPTY" }))
+                        }
+                    }
+                })
+            }
+            else {
+                res.send(JSON.stringify({ success: false, message: "order restaurantId missing" }))
+            }
+        })
+
+    }
+    else {
+        res.send(JSON.stringify({ success: false, message: "Wrong API Key" }))
+    }
+})
+
+
 router.get('/order', function (req, res, next) {
     if (req.query.key == API_KEY) {
 
         req.getConnection(function (error, conn) {
             var order_fbid = req.query.orderFBID
+            var startIndex = parseInt(req.query.from)
+            var endIndex = parseInt(req.query.to)
+
             if (order_fbid != null) {
                 conn.query('SELECT orderId,orderFBID,orderPhone,orderName,orderAddress,orderStatus,orderDate,'
                     + 'restaurantId,transactionId,'
                     + 'CASE WHEN cod=1 THEN \'TRUE\' ELSE \'FALSE\' END as cod,'
-                    + 'totalPRice,numOfItem FROM `Order` WHERE orderFBID =? AND numOfItem > 0'
+                    + 'totalPrice,numOfItem FROM `order` WHERE orderFBID =? AND numOfItem > 0'
+                    + ' ORDER By orderId DESC LIMIT ?,?', [order_fbid,startIndex,endIndex], function (err, rows, fields) {
+                    if (err) {
+                        res.status(500)
+                        res.send(JSON.stringify({ success: false, message: err.message }))
+                    }
+                    else {
+                        if (rows.length > 0) {
+                            res.send(JSON.stringify({ success: true, result: rows }))
+
+                        } else {
+                            res.send(JSON.stringify({ success: false, message: "_EMPTY" }))
+                        }
+                    }
+                })
+            }
+            else {
+                res.send(JSON.stringify({ success: false, message: "order fbId missing" }))
+            }
+        })
+
+    }
+    else {
+        res.send(JSON.stringify({ success: false, message: "Wrong API Key" }))
+    }
+})
+
+router.get('/maxorder', function (req, res, next) {
+    if (req.query.key == API_KEY) {
+
+        req.getConnection(function (error, conn) {
+            var order_fbid = req.query.orderFBID
+            
+            if (order_fbid != null) {
+                conn.query('SELECT COUNT(orderId) as maxRowNum  FROM `order` WHERE orderFBID =? AND numOfItem > 0'
                     + ' ORDER By orderId DESC', [order_fbid], function (err, rows, fields) {
                     if (err) {
                         res.status(500)
@@ -593,7 +780,7 @@ router.post('/createOrder', function (req, res, next) {
         var order_date = moment(req.body.orderDate, "MM/DD/YYYY").format("YYYY-MM-DD");
         var restaurant_id = req.body.restaurantId
         var transaction_id = req.body.transactionId
-        var cod = req.body.cod
+        var cod = (req.body.cod == "true")
         var total_price = req.body.totalPrice
         var num_of_item = req.body.numOfItem
         var order_fbid = req.body.orderFBID
@@ -601,20 +788,20 @@ router.post('/createOrder', function (req, res, next) {
 
         if (order_fbid != null) {
             req.getConnection(function (error, conn) {
-                conn.query('INSERT INTO `Order`(OrderFBID,OrderPhone,OrderName,OrderAddress,OrderStatus,OrderDate,RestaurantId,'
+                conn.query('INSERT INTO `order`(OrderFBID,OrderPhone,OrderName,OrderAddress,OrderStatus,OrderDate,RestaurantId,'
                     + 'TransactionId,COD,TotalPrice,NumOfItem) VALUES(?,?,?,?,?,?,?,?,?,?,?)', [order_fbid, order_phone, order_name, order_address, 0, order_date, restaurant_id, transaction_id, cod, total_price, num_of_item], function (err, rows, fields) {
                     if (err) {
                         res.status(500)
                         res.send(JSON.stringify({ success: false, message: err.message }))
                     }
                     else {
-                        conn.query('SELECT OrderId as orderNumber FROM `Order` WHERE OrderFBID=? AND NumOfItem > 0'
+                        conn.query('SELECT OrderId as orderNumber FROM `order` WHERE OrderFBID=? AND NumOfItem > 0'
                             + ' ORDER BY orderNumber DESC LIMIT 1', [order_fbid], function (err, rows, fields) {
                                 if (err) {
                                     res.status(500)
                                     res.send(JSON.stringify({ success: false, message: err.message }))
                                 } else {
-                                    res.send(JSON.stringify({ success: false, result: rows }))
+                                    res.send(JSON.stringify({ success: true, result: rows }))
                                 }
                             })
                     }
@@ -636,13 +823,49 @@ router.post('/createOrder', function (req, res, next) {
 //GET / POST
 //======================================
 
+router.get('/orderdetailbyrestaurant', function (req, res, next) {
+    if (req.query.key == API_KEY) {
+
+        req.getConnection(function (error, conn) {
+            var order_id = req.query.orderId
+            if (order_id != null) {
+                conn.query('SELECT orderdetail.orderId,itemId,quantity,size,addOn,orderFBID,name,description,image FROM orderdetail'
+                +' INNER JOIN `order` ON orderdetail.orderId=`order`.orderId'
+                +' INNER JOIN food On orderdetail.itemId=food.ID'
+                +' WHERE orderdetail.orderId=?', [order_id], function (err, rows, fields) {
+                        if (err) {
+                            res.status(500)
+                            res.send(JSON.stringify({ success: false, message: err.message }))
+                        }
+                        else {
+                            if (rows.length > 0) {
+                                res.send(JSON.stringify({ success: true, result: rows }))
+
+                            } else {
+                                res.send(JSON.stringify({ success: false, message: "_EMPTY" }))
+                            }
+                        }
+                    })
+            }
+            else {
+                res.send(JSON.stringify({ success: false, message: "order Id missing" }))
+            }
+        })
+
+    }
+    else {
+        res.send(JSON.stringify({ success: false, message: "Wrong API Key" }))
+    }
+})
+
+
 router.get('/orderDetails', function (req, res, next) {
     if (req.query.key == API_KEY) {
 
         req.getConnection(function (error, conn) {
             var order_id = req.query.orderId
             if (order_id != null) {
-                conn.query('SELECT orderId,itemId,quantity,discount,extraPrice,size,addOn FROM OrderDetail WHERE orderId=?', [order_id], function (err, rows, fields) {
+                conn.query('SELECT orderId,itemId,quantity,discount,extraPrice,size,addOn FROM orderdetail WHERE orderId=?', [order_id], function (err, rows, fields) {
                         if (err) {
                             res.status(500)
                             res.send(JSON.stringify({ success: false, message: err.message }))
@@ -676,10 +899,13 @@ router.post('/updateOrder', function (req, res, next) {
 
         try {
             order_detail = JSON.parse(req.body.orderDetail)
+console.log("parsing success")
         }
         catch (err) {
             res.status(500)
-            res.send(JSON.stringify({ success: false, message: err.message }))
+            console.log("parsing error")
+
+            res.send(JSON.stringify({ success: false, message: err.message}))
         }
 
         if (order_detail != null && order_id != null) {
@@ -698,10 +924,11 @@ router.post('/updateOrder', function (req, res, next) {
                     ]
             }
             req.getConnection(function (error, conn) {
-                conn.query('INSERT INTO OrderDetail(OrderId,ItemId,Quantity,Price,Discount,Size,Addon,ExtraPrice) VALUES(?)', data_insert , function (err, rows, fields) {
+                conn.query('INSERT INTO orderdetail(OrderId,ItemId,Quantity,Price,Discount,Size,Addon,ExtraPrice) VALUES(?)', data_insert, function (err, rows, fields) {
                         if (err) {
                             res.status(500)
-                            res.send(JSON.stringify({ success: false, message: err.message }))
+                            res.send(JSON.stringify({ success: false, message: err.message}))
+                            console.log("inserting"+parseInt(order_id))
                         }
                         else {
                             res.send(JSON.stringify({ success: true, message: "update success" }))
